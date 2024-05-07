a45dc0ec4f1da2f41ae19fe6a37d38f6637f047f
Kyle Preiksa <preiksa.kyle@gmail.com>
2024-05-07 19:34:45 -0400

    add yarn.lock

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Kyle\'s Test Documentation Site',
+  title: 'Kyle\'s Test Docusaurus Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
